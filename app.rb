3463d8570f5b7528b7ec31b457617188bc0b8a54
Alex Siegberg and Daniel Hongel <daniel@>
2019-07-22 16:41:23 -0700

    add full functionality to the program.

--- app.rb
+++ app.rb
@@ -2,3 +2,52 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/parcel')
 also_reload('lib/**/*.rb')
+
+get ('/') do
+  @parcels = Parcel.all
+  erb(:parcels)
+end
+
+get ('/parcels') do
+  @parcels = Parcel.all
+  erb(:parcels)
+end
+
+get ('/parcels/new') do
+  erb(:new_parcel)
+end
+
+post ('/parcels') do
+  name = params[:parcel_name]
+  width = params[:parcel_width]
+  height = params[:parcel_height]
+  weight = params[:parcel_weight]
+  parcel = Parcel.new(name, nil, width, height, weight)
+  parcel.save()
+  @parcels = Parcel.all()
+  erb(:parcels)
+end
+
+get ('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  erb(:parcel)
+end
+
+get ('/parcels/:id/edit') do
+  @parcel = Parcel.find(params[:id].to_i())
+  erb(:edit_parcel)
+end
+
+patch ('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  @parcel.update(params[:name])
+  @parcels = Parcel.all
+  erb(:parcels)
+end
+
+delete ('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  @parcel.delete()
+  @parcels = Parcel.all
+  erb(:parcels)
+end
--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,15 +1,50 @@
 require('pry')
 
 class Parcel
+  attr_reader :id
   attr_accessor :name, :width, :height, :weight
+  @@parcels = {}
+  @@total_rows = 0
 
-  def initialize(name, width, height, weight)
+  def initialize(name, id, width, height, weight)
     @name = name
     @width = width
     @height = height
     @weight = weight
+    @id = id || @@total_rows += 1
   end
 
+  def ==(parcel_to_compare)
+    self.name() == parcel_to_compare.name()
+  end
 
+  def save
+    @@parcels[self.id] = Parcel.new(self.name, self.id, self.width, self.height, self.weight)
+  end
+
+  def self.all()
+    @@parcels.values()
+  end
+
+  def self.find(name)
+  end
+
+  def self.clear
+    @@parcels = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@parcels[id]
+  end
+
+  def update(name)
+    self.name = name
+    @@parcels[self.id] = Parcel.new(self.name, self.id, self.width, self.height, self.weight)
+  end
+
+  def delete
+    @@parcels.delete(self.id)
+  end
 
 end
